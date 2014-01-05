80dc5568cec9529510a9d3b4024d7cae6eb57916
chenwenfeng <chenwenfeng1988@gmail.com>
2014-01-05 10:53:17 +0800

    complate template

--- controller.rb
+++ controller.rb
@@ -1,1 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+ignore /.gitignore/
+ignore /README/
+
 layout 'layout.html.haml'
+
+before 'index.html.haml' do
+  @title = "主页"
+  @navbar = "home"
+end
+
+before 'list.html.haml' do
+  @title = "列表"
+  @navbar = "list"
+end
